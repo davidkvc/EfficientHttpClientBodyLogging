d4f1a3c2c62cca5e2a8119f2fc8b4854bbc2ec01
David Kovac <daviz79@live.com>
2024-01-08 11:40:50 +0100

    add documentation for HttpBodyLoggingHandler

--- EfficientHttpClientBodyLogging/HttpBodyLoggingHandler.cs
+++ EfficientHttpClientBodyLogging/HttpBodyLoggingHandler.cs
@@ -8,22 +8,43 @@ using System.Threading.Tasks;
 
 namespace EfficientHttpClientBodyLogging;
 
+/// <summary>
+/// An implementation of <see cref="DelegatingHandler"/> that lazily
+/// logs request and response body.
+/// </summary>
 public class HttpBodyLoggingHandler : DelegatingHandler
 {
     private readonly HttpLoggingOptions _options;
     private readonly ILogger<HttpBodyLoggingHandler> _logger;
 
+    /// <summary>
+    /// Creates HttpBodyLoggingHandler with provided options and logger
+    /// </summary>
     public HttpBodyLoggingHandler(IOptions<HttpLoggingOptions> options, ILogger<HttpBodyLoggingHandler> logger)
     {
         _options = options.Value;
         _logger = logger;
     }
 
+    /// <inheritdoc/>
     protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (request.Content != null)
+        {
+            request.Content = HttpContentWrapper.WrapRequestContentForLogging(request.Content, _options, _logger);
+        }
+
+        var resp = base.Send(request, cancellationToken);
+
+        if (resp.Content != null)
+        {
+            resp.Content = HttpContentWrapper.WrapResponseContentForLogging(resp.Content, _options, _logger);
+        }
+
+        return resp;
     }
 
+    /// <inheritdoc/>
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         if (request.Content != null)
